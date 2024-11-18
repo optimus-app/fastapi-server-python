84a9a2a24af8a0511ca8ef118f7ede4cdbc43c9a
daniel chow <danielchow1020@gmail.com>
2024-11-18 16:19:41 +0800

    Adding various testcase

--- README_API.md
+++ README_API.md
@@ -1,3 +1,9 @@
 Startup the server
-- Run the below command in the most external directory that contains src
-- uvicorn src.pythonNewsAPI.main:app --reload
+1. Run the below command in your root directory that contains src
+    uvicorn src.pythonNewsAPI.main:app --reload
+
+2. Change the address to your own device address in below file, for testing
+    tests/pythonNewsAPI/test_newsAPI.py
+    
+3. Run the below command in your root directory that contains src
+    pytest tests/pythonNewsAPI/test_newsAPI.py -p no:warnings
--- tests/pythonNewsAPI/test_newsAPI.py
+++ tests/pythonNewsAPI/test_newsAPI.py
@@ -3,16 +3,28 @@ import logging
 from httpx import AsyncClient
 from src.pythonNewsAPI.main import app
 
+# Remove it to your Address
 BASE_URL = "http://127.0.0.1:8000"
 logging.basicConfig(level=logging.INFO)
 
 @pytest.mark.asyncio
 async def test_stock_news_polygon():
+
+    # Test with correct parameter
     async with AsyncClient(app=app, base_url = BASE_URL) as ac:
         response = await ac.post("/stock-news-polygon", json={
             "symbol": "AAPL",
             "order": "asc",
             "limit": "5"
+        })
+    #logging.info(f"Response Content: {response.json()}")
+    assert response.status_code == 200
+    assert "results" in response.json()
+
+    async with AsyncClient(app=app, base_url = BASE_URL) as ac:
+        response = await ac.post("/stock-news-polygon", json={
+            "symbol": "TSLA",
+            "order": "desc"
         })
     #logging.info(f"Response Content: {response.json()}")
     assert response.status_code == 200
@@ -25,7 +37,16 @@ async def test_stock_news_langchain():
             "symbol": "AAPL",
             "duration": "today"
         })
-    #logging.info(f"Response Content: {response.json()}")
+
+    assert response.status_code == 200
+    assert "result" in response.json()
+
+    async with AsyncClient(app=app, base_url = BASE_URL) as ac:
+        response = await ac.post("/stock-news-langchain", json={
+            "symbol": "AAPL",
+            "duration": "last week"
+        })
+
     assert response.status_code == 200
     assert "result" in response.json()
 
